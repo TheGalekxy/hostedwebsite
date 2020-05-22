f2d4f64571671879224913bb59253167708fab6d
Adam <galekxy13@hotmail.com>
2020-05-22 12:54:16 -0400

    Updates

---
+++ precache-manifest.ddaa608224fd0f1f91b9bd9f46df0f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee22b2940f000e157548fea427d3787",
+    "url": "/thegalekxy.github.io/index.html"
+  },
+  {
+    "revision": "41b8fcfa0088aca7ee86",
+    "url": "/thegalekxy.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19dc05e5734bdf766762",
+    "url": "/thegalekxy.github.io/static/js/2.39e49774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thegalekxy.github.io/static/js/2.39e49774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b8fcfa0088aca7ee86",
+    "url": "/thegalekxy.github.io/static/js/main.7f19eecc.chunk.js"
+  },
+  {
+    "revision": "eb8488b4a8a2c79af64a",
+    "url": "/thegalekxy.github.io/static/js/runtime-main.65d9b0ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thegalekxy.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
