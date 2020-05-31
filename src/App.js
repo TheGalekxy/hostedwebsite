1fff92784f3390be22934a1e859a7d8294f01029
Adam <galekxy13@hotmail.com>
2020-05-31 14:02:48 -0400

    testing changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1> Hello World. </h1>
         <p> Hosting a website is very easy with GitHub Pages! </p>
+        <p> Testing the custom domain</p>
       </header>
     </div>
   );
